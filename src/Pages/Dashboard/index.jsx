f2871d427a89a89f6b73039f28d31ef39e2c5af3
VYWL <dksu40@gmail.com>
2021-09-19 00:36:26 +0900

    Table Component 일부 구현 - //WIP

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,10 +1,15 @@
 import Card from '@Components/Card';
 import Bar from '@Components/Charts/Bar';
+import Table from '@Components/Table';
 import BarDummy from '@Dummy/barChartDummy';
 
 const Dashboard = () => {
     const { id, data, options } = BarDummy;
-    return <Card title='테이블'></Card>;
+    return (
+        <Card title='테이블'>
+            <Table />
+        </Card>
+    );
 };
 
 export default Dashboard;
