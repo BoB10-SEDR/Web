69d241438244506829ebdd39363421e0f07f9680
VYWL <dksu40@gmail.com>
2021-09-20 23:52:21 +0900

    일부 Component 개발 - Card 2개 형태 Component화 - List 2개 형태 Component화 - Line/Area Component화 진행중

--- src/Components/Charts/Line/index.jsx
+++ src/Components/Charts/Line/index.jsx
@@ -1,5 +1,117 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import '@Styles/chart.css';
+import { Chart } from 'chart.js';
+import { useEffect } from 'react';
+
 const LineArea = () => {
-    return <div>I'm Line Component</div>;
+    // TODO :: data, Option에 대한 템플릿화가 필요.
+    const data = {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [
+            {
+                label: 'Earnings',
+                lineTension: 0.4,
+                backgroundColor: 'rgba(78, 115, 223, 0.05)',
+                borderColor: 'rgba(78, 115, 223, 1)',
+                pointRadius: 3,
+                pointBackgroundColor: 'rgba(78, 115, 223, 1)',
+                pointBorderColor: 'rgba(78, 115, 223, 1)',
+                pointHoverRadius: 3,
+                pointHoverBackgroundColor: 'rgba(78, 115, 223, 1)',
+                pointHoverBorderColor: 'rgba(78, 115, 223, 1)',
+                pointHitRadius: 10,
+                pointBorderWidth: 2,
+                data: [0, 50000, 5000, 15000, 10000, 20000, 15000, 25000, 20000, 30000, 25000, 40000],
+            },
+        ],
+    };
+
+    const option = {
+        maintainAspectRatio: false,
+        layout: {
+            padding: {
+                left: 10,
+                right: 25,
+                top: 25,
+                bottom: 0,
+            },
+        },
+        scales: {
+            xAxes: [
+                {
+                    time: {
+                        unit: 'date',
+                    },
+                    gridLines: {
+                        display: false,
+                        drawBorder: false,
+                    },
+                    ticks: {
+                        maxTicksLimit: 7,
+                    },
+                },
+            ],
+            yAxes: [
+                {
+                    ticks: {
+                        maxTicksLimit: 5,
+                        padding: 10,
+                        // Include a dollar sign in the ticks
+                        callback: function (value, index, values) {
+                            return '$' + value.toLocaleString();
+                        },
+                    },
+                    gridLines: {
+                        color: 'rgb(234, 236, 244)',
+                        zeroLineColor: 'rgb(234, 236, 244)',
+                        drawBorder: false,
+                        borderDash: [2],
+                        zeroLineBorderDash: [2],
+                    },
+                },
+            ],
+        },
+        legend: {
+            display: false,
+        },
+        tooltips: {
+            backgroundColor: 'rgb(255,255,255)',
+            bodyFontColor: '#858796',
+            titleMarginBottom: 10,
+            titleFontColor: '#6e707e',
+            titleFontSize: 14,
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            intersect: false,
+            mode: 'index',
+            caretPadding: 10,
+            callbacks: {
+                label: function (tooltipItem, chart) {
+                    var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+                    return datasetLabel + '$: ' + tooltipItem.yLabel.toLocaleString();
+                },
+            },
+        },
+    };
+
+    useEffect(() => {
+        lineChartInit(data, option);
+    }, []);
+
+    // TODO :: id, className, 그 이외의 설정들에 대한 컴포넌트화가 진행중임.
+    return (
+        <div className='lineChartArea'>
+            <canvas id='lineChart' />
+        </div>
+    );
 };
 
 export default LineArea;
+
+const lineChartInit = (data, option) => {
+    var ctx = document.getElementById('lineChart');
+    new Chart(ctx, { type: 'line', data: data, options: option });
+};
