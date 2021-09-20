6bc3c1b528e815a9f570a261cd8886454a54d722
Jiwon Pyo <70142731+VYWL@users.noreply.github.com>
2021-09-20 10:54:10 -0400

    일부 Component 개발 (#5)
    - Card 2개 형태 Component화
    - List 2개 형태 Component화
    - Line/Area Component화 진행중

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '@Styles/index.css';
 
-const Home = () => {
-    return <div>hello</div>;
-};
+// Components
+import Card from '@Components/Card';
+import LineArea from '@Components/Charts/Line';
 
+const Home = () => {
+    return (
+        <div className='main'>
+            <div id='bodySection'>
+                <Card title='라인 그래프'>
+                    <LineArea />
+                </Card>
+            </div>
+        </div>
+    );
+};
 ReactDOM.render(<Home />, document.getElementById('root'));
