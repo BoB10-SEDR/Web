e88bd6f390a1a056b778700a70349aa4f2feb98c
hyesong kim <wisepine@kakao.com>
2021-09-23 14:31:56 +0900

    feat: Pie chart component 구현

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,13 +1,22 @@
 import Card from '@Components/Card';
 import Bar from '@Components/Charts/Bar';
+import Pie from '@Components/Charts/Pie';
 import BarDummy from '@Dummy/barChartDummy';
+import PieDummy from '@Dummy/pieChartDummy';
 
 const Dashboard = () => {
-    const { id, data, options } = BarDummy;
+    const { barId, barData, barOptions } = BarDummy;
+    const { pieId, pieData, pieOptions } = PieDummy;
+
     return (
-        <Card title='바 차트'>
-            <Bar id={id} data={data} options={options}></Bar>
-        </Card>
+        <div id='dashboard'>
+            <Card title='바 차트'>
+                <Bar id={barId} data={barData} options={barOptions} />
+            </Card>
+            <Card title='파이 차트'>
+                <Pie id={pieId} data={pieData} options={pieOptions} />
+            </Card>
+        </div>
     );
 };
 
