1c5228427e335604a60d411b30034e51dc9b5bdc
hyesong kim <wisepine@kakao.com>
2021-09-24 19:58:38 +0900

    refactor: dropDownMenu css 분리

--- src/Components/UI/DropDownMenu/index.jsx
+++ src/Components/UI/DropDownMenu/index.jsx
@@ -1,5 +1,13 @@
+import '@Styles/dropDownMenu.css';
+
 const DropDownMenu = props => {
-    return null;
+    return (
+        <div class='dropdown'>
+            <a className='dropdownToggle'>
+                <SVG name='threeDotsButton' height='16px' width='16px' />
+            </a>
+        </div>
+    );
 };
 
 export default DropDownMenu;
