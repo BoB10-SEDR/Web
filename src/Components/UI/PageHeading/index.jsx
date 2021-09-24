a88949c1104cf244952e7959a8ac9ee279459f3e
VYWL <dksu40@gmail.com>
2021-09-24 17:29:39 +0900

    PenTesting Layout 추가 - PageHeading에 noToggle Property 추가. - Dummy에 이미지 관련 컴포넌트 추가

--- src/Components/UI/PageHeading/index.jsx
+++ src/Components/UI/PageHeading/index.jsx
@@ -1,16 +1,18 @@
 import '@Styles/pageHeading.css';
 
 const PageHeading = props => {
-    const { title = 'Untitled' } = props;
+    const { title = 'Untitled', noToggle = false } = props;
 
     return (
         <div className='pageHeading'>
             <h1 className='pageName'>{title}</h1>
 
-            <a href='#' className='generateReportButton'>
-                <i className='fas fa-download fa-sm text-white-50'></i>
-                {` Generate Report`}
-            </a>
+            {!noToggle && (
+                <a href='#' className='generateReportButton'>
+                    <i className='fas fa-download fa-sm text-white-50'></i>
+                    {` Generate Report`}
+                </a>
+            )}
         </div>
     );
 };
