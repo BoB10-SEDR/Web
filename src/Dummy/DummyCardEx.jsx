acc7a4f7aa81d81d8ee64098fbd7c33d4c3eeedf
Hyesong Kim <41888956+lauvsong@users.noreply.github.com>
2021-09-28 23:05:21 +0900

    Added/layout/logs (#36)
    * fix: ToggleSwitch id 중복
    
    * fix: Logs 하위 페이지 경로 설정
    
    * feat: Detected Layout 구현
    
    * style: mb(margin-bottom) 클래스 스타일 속성
    
    * feat: DummyCardEx component
    
    * feat: Devices layout
    
    * refactor: page style
    
    * feat: Newtwork layout
    
    Co-authored-by: hyesong kim <wisepine@kakao.com>

---
+++ src/Dummy/DummyCardEx.jsx
@@ -0,0 +1,6 @@
+const DummyCardEx = props => {
+    const { width = '100%', height } = props;
+    return <div style={{ width: width, height: height }} />;
+};
+
+export default DummyCardEx;
--- src/Pages/Logs/Devices/index.jsx
+++ src/Pages/Logs/Devices/index.jsx
@@ -1,28 +1,59 @@
-import '@Styles/devices.css';
 import { Row, Col } from '@Components/Grid';
 
+import PageHeading from '@Components/UI/PageHeading';
 import Card from '@Components/Card';
 import BarDummy from '@Dummy/barChartDummy';
 import PieDummy from '@Dummy/pieChartDummy';
+import DummyCardSimple from '@Dummy/DummyCardSimple';
+import DummyCardEx from '@Dummy/DummyCardEx';
 
 const Devices = () => {
     const { barId, barData, barOptions } = BarDummy;
     const { pieId, pieData, pieOptions } = PieDummy;
 
     return (
-        <div id='detected' className='logDetail'>
+        <div id='detected' className='page'>
+            <PageHeading title='Devices' />
             <Row>
-                <Col xl={3} md={6} mb>
-                    <Row>{/* <Card children={}></Card> */}</Row>
-                    <Row>
-                        <Card></Card>
-                    </Row>
-                    <Row>
-                        <Card></Card>
-                    </Row>
+                <Col xl={3} md={3} mb>
+                    <Card>
+                        <DummyCardSimple></DummyCardSimple>
+                    </Card>
+                    <Card>
+                        <DummyCardSimple></DummyCardSimple>
+                    </Card>
+                    <Card>
+                        <DummyCardSimple></DummyCardSimple>
+                    </Card>
                 </Col>
-                <Col xl={9} md={6} mb>
-                    <Card></Card>
+                <Col xl={9} md={9} mb>
+                    <Card>
+                        <DummyCardEx height='300px'></DummyCardEx>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={6} mb>
+                    <Row>
+                        <Col md={6} mb>
+                            <Card>
+                                <DummyCardEx height='200px'></DummyCardEx>
+                            </Card>
+                        </Col>
+                        <Col md={6} mb>
+                            <Card>
+                                <DummyCardEx height='200px'></DummyCardEx>
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Card>
+                        <DummyCardEx height='234px'></DummyCardEx>
+                    </Card>
+                </Col>
+                <Col md={6} mb>
+                    <Card>
+                        <DummyCardEx height='500px' />
+                    </Card>
                 </Col>
             </Row>
         </div>
