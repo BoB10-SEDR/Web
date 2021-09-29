2c6f5dd85c4edec08a8d72f28a17dd41497664b6
hyesong kim <wisepine@kakao.com>
2021-09-29 12:09:55 +0900

    feat: Device 카드 배치

---
+++ src/Components/DeviceInfo/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { Row, Col } from '@Components/Grid';
+
+const DeviceInfo = () => {
+    return (
+        <div id='deviceInfo'>
+            <div className='wrapper'></div>
+        </div>
+    );
+};
+
+export default DeviceInfo;
