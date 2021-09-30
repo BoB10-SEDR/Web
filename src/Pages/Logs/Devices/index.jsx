bc06e51b4215c1abb3f268f4105d4c7515ab180d
Hyesong Kim <41888956+lauvsong@users.noreply.github.com>
2021-09-30 15:11:35 +0900

    Added/layout/devices (#37)
    * fix: ToggleSwitch id 중복
    
    * fix: Logs 하위 페이지 경로 설정
    
    * feat: Detected Layout 구현
    
    * style: mb(margin-bottom) 클래스 스타일 속성
    
    * feat: DummyCardEx component
    
    * feat: Devices layout
    
    * refactor: page style
    
    * feat: Newtwork layout
    
    * feat: Device 카드 배치
    
    * feat: Device main 배치
    
    * feat: DeviceInfo
    - //WIP
    
    * refactor: viewType 등호 연산자 수정
    
    Co-authored-by: hyesong kim <wisepine@kakao.com>

--- src/Pages/Logs/Devices/index.jsx
+++ src/Pages/Logs/Devices/index.jsx
@@ -2,6 +2,7 @@ import { Row, Col } from '@Components/Grid';
 
 import PageHeading from '@Components/UI/PageHeading';
 import Card from '@Components/Card';
+import DeviceInfo from '@Components/DeviceInfo';
 import BarDummy from '@Dummy/barChartDummy';
 import PieDummy from '@Dummy/pieChartDummy';
 import DummyCardSimple from '@Dummy/DummyCardSimple';
@@ -15,47 +16,24 @@ const Devices = () => {
         <div id='detected' className='page'>
             <PageHeading title='Devices' />
             <Row>
-                <Col xl={3} md={3} mb>
+                <Col md={3} mb>
                     <Card>
                         <DummyCardSimple></DummyCardSimple>
                     </Card>
                     <Card>
                         <DummyCardSimple></DummyCardSimple>
                     </Card>
-                    <Card>
-                        <DummyCardSimple></DummyCardSimple>
+                    <Card title='공격 유형별 로그 현황'>
+                        <DummyCardEx height='215px'></DummyCardEx>
                     </Card>
                 </Col>
-                <Col xl={9} md={9} mb>
+                <Col md={9} mb>
                     <Card>
-                        <DummyCardEx height='300px'></DummyCardEx>
+                        <DummyCardEx height='500px'></DummyCardEx>
                     </Card>
                 </Col>
             </Row>
-            <Row>
-                <Col md={6} mb>
-                    <Row>
-                        <Col md={6} mb>
-                            <Card>
-                                <DummyCardEx height='200px'></DummyCardEx>
-                            </Card>
-                        </Col>
-                        <Col md={6} mb>
-                            <Card>
-                                <DummyCardEx height='200px'></DummyCardEx>
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Card>
-                        <DummyCardEx height='234px'></DummyCardEx>
-                    </Card>
-                </Col>
-                <Col md={6} mb>
-                    <Card>
-                        <DummyCardEx height='500px' />
-                    </Card>
-                </Col>
-            </Row>
+            <DeviceInfo />
         </div>
     );
 };
