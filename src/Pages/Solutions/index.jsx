064f4636c5901e96c1af71cbfdcd139fc7774f87
hyesong kim <wisepine@kakao.com>
2021-10-04 14:06:12 +0900

    feat: Solutions layout

--- src/Pages/Solutions/index.jsx
+++ src/Pages/Solutions/index.jsx
@@ -1,5 +1,27 @@
+import PageHeading from '@Components/UI/PageHeading';
+import Card from '@Components/Card';
+import Table from '@Components/Table';
+import Button from '@Components/UI/Button';
+
 const Solutions = () => {
-    return null;
+    return (
+        <div id='solutions' className='page'>
+            <PageHeading title='Solutions' />
+            <Card>
+                <TableHeading />
+                <Table />
+            </Card>
+        </div>
+    );
+};
+
+const TableHeading = () => {
+    return (
+        <div className='tableHeading'>
+            <span>대응 정책</span>
+            <Button>Apply</Button>
+        </div>
+    );
 };
 
 export default Solutions;
