c7c87949ed36aff78971d1b3d9c2f395209d584e
VYWL <dksu40@gmail.com>
2021-10-05 17:44:25 +0900

    Slider 스타일 일부 제외 완료 - @brainhubeu/react-carousel를 사용했음 - dummy data 마련 해뒀음 - 화살표 일부 custom 필요

--- src/Components/Slider/index.jsx
+++ src/Components/Slider/index.jsx
@@ -1,27 +1,52 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
+import '@Styles/slider.css';
 
 import Carousel, { Dots } from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
+import Item from './Item';
 
 const Slider = props => {
-    const [nowState, setState] = useState({
-        value: 0,
-        slides: [<div>1</div>, <div>2</div>, <div>3</div>],
-    });
+    const { data = [] } = props;
+
+    // TODO_P :: data 가공해야함 => pagenation (8개 단위로)
+
+    const [nowTabIndex, setTabIndex] = useState(0);
+
+    const [nowModuleData, setModuleData] = useState([]);
+
+    const [nowItemIndex, setItemIndex] = useState(0);
+
+    const dummyList = [1, 2, 3, 4, 5];
+
+    useEffect(() => {
+        // TODO_P :: Backend API 연동
+        // setModuleData(getModuleData())
+    }, []);
 
     const handleChange = value => {
-        console.log(value);
-        setState(p => {
-            return { ...p, value: value };
-        });
+        setTabIndex(value);
+        setItemIndex(0);
     };
 
     return (
-        <Carousel plugins={['arrows']}>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-        </Carousel>
+        <>
+            <Carousel
+                value={nowTabIndex}
+                onChange={handleChange}
+                plugins={['arrows']}
+                draggable={false}
+                animationSpeed={200}
+            >
+                {dummyList.map(e => (
+                    <Item data={nowModuleData} nowSelected={nowItemIndex} setSelectedItemIndex={setItemIndex} />
+                ))}
+            </Carousel>
+
+            <div className='dotsWrapper'>
+                <Dots value={nowTabIndex} onChange={handleChange} number={dummyList.length} />
+                <div className='searchResult'>모듈 총 {40}개</div>
+            </div>
+        </>
     );
 };
 
