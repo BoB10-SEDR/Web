a1f69303ea8ac1fe6788472645a3aa9359e2779f
hyesong kim <wisepine@kakao.com>
2021-10-07 06:15:13 +0900

    refactor: Nav 컴포넌트 리팩토링

--- src/Components/Nav/index.jsx
+++ src/Components/Nav/index.jsx
@@ -1,22 +1,38 @@
 import '@Styles/nav.css';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { IoIosArrowBack } from 'react-icons/io';
 import Item from '@Components/Nav/Item';
+import Button from '@Components/UI/Button';
 
 const Nav = props => {
     const { brand, items } = props;
 
     return (
         <ul className='nav'>
-            <Link className='navBrand' to={brand.path}>
-                {brand.icon}
-                <span>{brand.title}</span>
-            </Link>
+            <NavHeader brand={brand} />
             <hr className='navDivider navXmargin' />
-            {items.map(item => {
-                return <Item type={item.type} item={item} />;
-            })}
+            <NavBody items={items} />
         </ul>
     );
+};
+
+const NavHeader = props => {
+    const { brand } = props;
+
+    return (
+        <Link id='navHeader' to={brand.path}>
+            {brand.icon}
+            <span>{brand.title}</span>
+        </Link>
+    );
+};
+
+const NavBody = props => {
+    const { items } = props;
+    return items.map(item => {
+        return <Item type={item.type} item={item} />;
+    });
 };
 
 export default Nav;
