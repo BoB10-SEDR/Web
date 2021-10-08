42c5b7f3bec166f72e861d2ef73d2e4e47f6b455
hyesong kim <wisepine@kakao.com>
2021-10-08 21:13:15 +0900

    feat: ProfileStamp 구현

--- src/Components/ModuleInfo/index.jsx
+++ src/Components/ModuleInfo/index.jsx
@@ -54,8 +54,19 @@ const ProfileStamp = () => {
     return (
         <div className='profileStamp'>
             <DeviceIcon size={100} isActive />
+            <ModuleName />
+            <ModuleId id={'2019003709'} />
         </div>
     );
+};
+
+const ModuleName = () => {
+    return <div className='moduleName'>모듈 이름</div>;
+};
+
+const ModuleId = props => {
+    const { id } = props;
+    return <div className='moduleId'>ID: {id}</div>;
 };
 
 const ProfileDetail = () => {
