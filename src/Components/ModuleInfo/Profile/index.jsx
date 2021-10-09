94ea5cf3f5426ba932b5c0d536010b41badf5b0b
hyesong kim <wisepine@kakao.com>
2021-10-09 12:55:00 +0900

    refactor: Header 합침

--- src/Components/ModuleInfo/Profile/index.jsx
+++ src/Components/ModuleInfo/Profile/index.jsx
@@ -1,6 +1,7 @@
 import '@Styles/moduleInfo.css';
-import Header from './Header';
 import Body from './Body';
+import KebabMenu from '@Components/UI/KebabMenu';
+import KebabDummy from '@Dummy/KebabDummy';
 
 const Profile = props => {
     const { data } = props;
@@ -9,6 +10,16 @@ const Profile = props => {
         <div className='moduleProfile'>
             <Header />
             <Body data={data} />
+        </div>
+    );
+};
+
+const Header = () => {
+    return (
+        <div className='profileHeader'>
+            <div className='profileMenu'>
+                <KebabMenu color='#97989b' items={KebabDummy} />
+            </div>
         </div>
     );
 };
