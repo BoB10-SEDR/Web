b784b6e214fc1df35967fcf4b5e7217726e2b180
hyesong kim <wisepine@kakao.com>
2021-10-10 18:49:54 +0900

    feat: checkBox 컴포넌트 구현

--- src/Components/UI/CheckBox/index.jsx
+++ src/Components/UI/CheckBox/index.jsx
@@ -1,11 +1,20 @@
-const CheckBox = props => {
-    const { isChecked } = props;
+import '@Styles/ui.css';
+import { forwardRef, useEffect, useRef } from 'react';
+
+const CheckBox = forwardRef(({ indeterminate, ...rest }, ref) => {
+    const defaultRef = useRef();
+    const resolvedRef = ref || defaultRef;
+
+    useEffect(() => {
+        resolvedRef.current.indeterminate = indeterminate;
+    }, [resolvedRef, indeterminate]);
 
     return (
-        <div className={'checkBox' + (isChecked && ' checked')}>
-            <input type='checkbox' />
-        </div>
+        <label className='checkBox'>
+            <input type='checkbox' ref={resolvedRef} {...rest} />
+            <span className='checkMark'></span>
+        </label>
     );
-};
+});
 
 export default CheckBox;
