c5a724e1686c7f4d09796e742a29ad299907a943
hyesong kim <wisepine@kakao.com>
2021-10-12 13:26:36 +0900

    feat: Details 페이지 dummy 채움

--- src/Pages/Logs/Details/index.jsx
+++ src/Pages/Logs/Details/index.jsx
@@ -1,23 +1,28 @@
+import { useEffect } from 'react';
 import { Row, Col } from '@Components/Grid';
 
 import PageHeading from '@Components/UI/PageHeading';
 import Card from '@Components/Card';
-import DeviceNav from '@Components/DeviceNav';
+import CardBodyForm from '@Components/Card/Form';
 import EventViewer from '@Components/EventViewer';
 import ModuleInfo from '@Components/ModuleInfo';
+import UpDown from '@Components/UI/UpDown';
+import Table from '@Components/Table';
+import DeviceNav from '@Components/DeviceNav';
+import Pie from '@Components/Charts/Pie';
+import Gauge from '@Components/Charts/Gauge';
 
-import BarDummy from '@Dummy/barChartDummy';
 import PieDummy from '@Dummy/pieChartDummy';
 import DummyCardSimple from '@Dummy/DummyCardSimple';
 import DummyCardEx from '@Dummy/DummyCardEx';
+import { dummyColumns, dummyData } from '@Dummy/solutionTableDummy';
+import d from '@Dummy/dummyDetails';
 
 const Networks = () => {
-    const { barId, barData, barOptions } = BarDummy;
     const { pieId, pieData, pieOptions } = PieDummy;
 
     return (
         <div id='networks' className='page'>
-            <PageHeading title='Details' />
             <Row>
                 <Col xl={1} md={1}>
                     <DeviceNav />
@@ -26,11 +31,20 @@ const Networks = () => {
                     <Card>
                         <Row>
                             <Col xl={3} md={3} mb>
-                                <Card>
-                                    <DummyCardSimple></DummyCardSimple>
+                                <Card border='1px solid red'>
+                                    <CardBodyForm titleFontColor='red' title='전체 로그 개수' content='19,991,006개' />
+                                    <UpDown isDecreasing={true} amount={d.totalLogs.toLocaleString()} />
                                 </Card>
-                                <Card>
-                                    <DummyCardSimple></DummyCardSimple>
+                                <Card border='1px solid red'>
+                                    <CardBodyForm
+                                        titleFontColor='red'
+                                        title='공격 유형 / 공격 로그 개수'
+                                        content='19,991,006개'
+                                    />
+                                    <UpDown
+                                        isDecreasing={true}
+                                        amount={`${d.attacks} / ${d.totalLogs.toLocaleString()}`}
+                                    />
                                 </Card>
                             </Col>
                             <Col xl={9} md={9} mb>
@@ -41,18 +55,22 @@ const Networks = () => {
                         </Row>
                         <Row>
                             <Col md={3} mb>
-                                <Card>
-                                    <DummyCardEx height='200px'></DummyCardEx>
+                                <Card title='공격 유형별 로그 개수'>
+                                    <DummyCardEx height='277px'>
+                                        <Gauge />
+                                    </DummyCardEx>
                                 </Card>
                             </Col>
                             <Col md={3} mb>
-                                <Card>
-                                    <DummyCardEx height='200px'></DummyCardEx>
+                                <Card title='공격 유형별 로그 비율'>
+                                    <Pie id={pieId} data={pieData} options={pieOptions} />
                                 </Card>
                             </Col>
                             <Col md={6} mb>
-                                <Card>
-                                    <DummyCardEx height='200px' />
+                                <Card title='적용된 대응 정책 리스트'>
+                                    <DummyCardEx height='277px'>
+                                        <Table columns={dummyColumns} data={dummyData} />
+                                    </DummyCardEx>
                                 </Card>
                             </Col>
                             <Col md={12} mb>
