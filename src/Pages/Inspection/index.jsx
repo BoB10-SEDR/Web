969ca6861310d03f2c444a19fcff4a6cd33f6aad
Jiwon Pyo <70142731+VYWL@users.noreply.github.com>
2021-10-14 14:29:11 +0900

    Fix/layout/inspection (#83)
    * Dashboard 수정
    - Layout 손봤음
    
    * Inspection Page Layout 수정중
    - //WIP

---
+++ src/Pages/Inspection/index.jsx
@@ -0,0 +1,34 @@
+import Card from '@Components/Card';
+import { Row, Col } from '@Components/Grid';
+import Table from '@Components/Table';
+import PageHeading from '@Components/UI/PageHeading';
+import '@Styles/inspection.css';
+import { DummyMockTest, DummyHistory } from '@Dummy/DummyImgs.jsx';
+
+const Inspection = () => {
+    return (
+        <div id='inspection'>
+            {/* <PageHeading title='Pen-testing' noToggle /> */}
+
+            <Row>
+                <Col lg={12}>
+                    <Card title='Checklist'>
+                        {/* TODO_P :: MockTest 관련 Table */}
+                        <DummyMockTest />
+                    </Card>
+                </Col>
+            </Row>
+
+            <Row>
+                <Col lg={12}>
+                    <Card title='History'>
+                        {/* TODO_P :: History 관련 Table */}
+                        <DummyHistory />
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    );
+};
+
+export default Inspection;
