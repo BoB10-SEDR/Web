774f0716b25e2ccb5121c054f22a4c87ceb850d4
hyesong kim <wisepine@kakao.com>
2021-10-17 03:05:57 +0900

    refactor: Details 페이지 Heading 제거

--- src/Pages/Logs/Details/index.jsx
+++ src/Pages/Logs/Details/index.jsx
@@ -1,14 +1,11 @@
 import { useEffect } from 'react';
 import { Row, Col } from '@Components/Grid';
-
-import PageHeading from '@Components/UI/PageHeading';
 import Card from '@Components/Card';
 import CardBodyForm from '@Components/Card/Form';
 import EventViewer from '@Components/EventViewer';
 import ModuleInfo from '@Components/ModuleInfo';
 import UpDown from '@Components/UI/UpDown';
 import Table from '@Components/Table';
-import DeviceNav from '@Components/DeviceNav';
 import Pie from '@Components/Charts/Pie';
 import Gauge from '@Components/Charts/Gauge';
 
@@ -19,12 +16,11 @@ import { dummyColumns, dummyData } from '@Dummy/solutionTableDummy';
 import d from '@Dummy/dummyDetails';
 
 const Details = props => {
-    const { title } = props;
+    const { path } = props;
     const { pieId, pieData, pieOptions } = PieDummy;
 
     return (
         <div id='details'>
-            <PageHeading title={title} />
             <Row>
                 <Col xl={3} md={3} mb>
                     <Card border='1px solid red'>
