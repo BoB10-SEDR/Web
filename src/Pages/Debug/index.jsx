161ec01fb2723a6b9aa3053bf2a6688de79848fc
VYWL <dksu40@gmail.com>
2021-10-21 14:30:45 +0900

    Debug 페이지 비웠음

--- src/Pages/Debug/index.jsx
+++ src/Pages/Debug/index.jsx
@@ -1,29 +1,10 @@
-import Card from '@Components/Card';
 import { Row, Col } from '@Components/Grid';
-import List from '@Components/List';
-import Table from '@Components/Table';
-import { Status } from '@Components/UI';
-
 import '@Styles/debug.css';
 
 const Debug = () => {
-    const dummyItems = [
-        { itemType: 'toggle', name: 'SQL Injection', desc: 'A, B, C, D에 적용 됨' },
-        { itemType: 'toggle', name: '이러이러한 공격', desc: 'A, B, C, D에 적용 됨' },
-        { itemType: 'toggle', name: '짧', desc: 'A, B, C, D에 적용 됨' },
-        { itemType: 'toggle', name: '그 외 나머지', desc: 'A, B, C, D에 적용 됨' },
-    ];
-
     return (
         <div id='debug' className='page'>
             {/* 작업 알아서 하시길 바랍니다 */}
-            <Card title='안녕'>
-                <Row>
-                    <Col lg={3}>
-                        <List title='공격 유형 별 로그 비율' titleFontColor='#F02632' content={dummyItems} />
-                    </Col>
-                </Row>
-            </Card>
         </div>
     );
 };
