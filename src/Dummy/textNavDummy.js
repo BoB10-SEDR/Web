6981ca4a95921b2c2d3afb16d7dfc81b492e0eff
Hyesong Kim <41888956+lauvsong@users.noreply.github.com>
2021-10-24 12:34:00 +0900

    Add/text nav (#104)

--- src/Dummy/textNavDummy.js
+++ src/Dummy/textNavDummy.js
@@ -6,76 +6,91 @@ import { HiOutlineServer } from 'react-icons/hi';
 
 const dummy = [
     {
+        id: 0,
         Icon: BiHome,
         title: 'Home',
         count: 6,
     },
     {
+        id: 1,
         Icon: MdAttachMoney,
         title: '계산대',
         count: 8,
     },
     {
+        id: 2,
         Icon: IoFastFoodOutline,
         title: '진열대',
         count: 5,
     },
     {
+        id: 3,
         Icon: RiDoorLine,
         title: '출입구',
         count: 3,
     },
     {
+        id: 4,
         Icon: HiOutlineServer,
         title: '서버실',
         count: 2,
     },
     {
+        id: 5,
         Icon: BiHome,
         title: 'Home',
         count: 6,
     },
     {
+        id: 6,
         Icon: MdAttachMoney,
         title: '계산대',
         count: 8,
     },
     {
+        id: 7,
         Icon: IoFastFoodOutline,
         title: '진열대',
         count: 5,
     },
     {
+        id: 8,
         Icon: RiDoorLine,
         title: '출입구',
         count: 3,
     },
     {
+        id: 9,
         Icon: HiOutlineServer,
         title: '서버실',
         count: 2,
     },
     {
+        id: 10,
         Icon: BiHome,
         title: 'Home',
         count: 6,
     },
     {
+        id: 11,
         Icon: MdAttachMoney,
         title: '계산대',
         count: 8,
     },
     {
+        id: 12,
         Icon: IoFastFoodOutline,
         title: '진열대',
         count: 5,
     },
     {
+        id: 13,
         Icon: RiDoorLine,
         title: '출입구',
         count: 3,
     },
     {
+        id: 14,
         Icon: HiOutlineServer,
         title: '서버실',
         count: 2,
