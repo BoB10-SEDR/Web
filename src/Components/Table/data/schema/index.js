dfbe56d1ed674d41effccfbd41e2daac8b980ccd
VYWL <dksu40@gmail.com>
2021-11-09 04:45:34 +0900

    모든 Table schema 추가

--- src/Components/Table/data/schema/index.js
+++ src/Components/Table/data/schema/index.js
@@ -12,3 +12,49 @@ export const sparklineSchema = [
     { Header: '총 로그 수', accessor: 'total', align: 'left', isAbleToSort: true },
     { Header: '변화 추이', accessor: 'trend', align: 'left', isAbleToSort: true },
 ];
+
+// TODO_P :: 간격 배정
+export const deviceTableSchema = [
+    { Header: '장치 종류', accessor: 'deviceIcon', align: 'center', isAbleToSort: false },
+    { Header: '장치 이름', accessor: 'name', align: 'center', isAbleToSort: true },
+    { Header: '소속 구성', accessor: 'group', align: 'center', isAbleToSort: true },
+    { Header: '식별번호', accessor: 'serialNum', align: 'center', isAbleToSort: true },
+    { Header: '트래픽 변화량', accessor: 'traficDiff', align: 'center', isAbleToSort: true },
+    { Header: '연결 상태', accessor: 'status', align: 'center', isAbleToSort: true },
+    { Header: '연결 모듈 수', accessor: 'moduleCount', align: 'center', isAbleToSort: true },
+    { Header: '상세 정보', accessor: 'more', align: 'center', isAbleToSort: false },
+];
+
+// TODO_P :: 간격 배정
+export const eventViewerSchema = [
+    { Header: '타임스탬프', accessor: 'timestamp', align: 'center', isAbleToSort: true },
+    { Header: '로그 상태', accessor: 'status', align: 'center', isAbleToSort: true },
+    { Header: '로그 발생지', accessor: 'source', align: 'center', isAbleToSort: true },
+    { Header: '이벤트 ID', accessor: 'eventID', align: 'center', isAbleToSort: true },
+    { Header: '상세 설명', accessor: 'description', align: 'left', isAbleToSort: false },
+];
+
+// TODO_P :: 간격 배정
+export const applyedPolicyListSchema = [
+    { Header: '대응 정책 명', accessor: 'policyName', align: 'left', isAbleToSort: false },
+    { Header: '상세 설명', accessor: 'description', align: 'left', isAbleToSort: false },
+    { Header: '적용 여부', accessor: 'status', align: 'center', isAbleToSort: true },
+];
+
+// TODO_P :: 간격 배정
+export const recommandedPolicyListSchema = [
+    { Header: '대응 정책 명', accessor: 'policyName', align: 'left', isAbleToSort: false },
+    { Header: '상세 설명', accessor: 'description', align: 'left', isAbleToSort: false },
+    { Header: '타임스탬프', accessor: 'timestamp', align: 'left', isAbleToSort: false },
+    { Header: '적용 여부', accessor: 'status', align: 'center', isAbleToSort: false },
+];
+
+// TODO_P :: 간격 배정
+export const inspectionTableSchema = [
+    { Header: '선택', accessor: 'check', align: 'center', isAbleToSort: false },
+    { Header: '테스트 ID', accessor: 'testID', align: 'left', isAbleToSort: false },
+    { Header: '테스트 이름', accessor: 'testName', align: 'left', isAbleToSort: false },
+    { Header: '테스트 대상 유형', accessor: 'target', align: 'left', isAbleToSort: false },
+    { Header: '대상 OS', accessor: 'target', align: 'left', isAbleToSort: false },
+    { Header: '테스트 설명', accessor: 'description', align: 'left', isAbleToSort: false },
+];
