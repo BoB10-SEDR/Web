83629ea1296ba902388b1085ac9cb0b1112af28c
hyesong kim <wisepine@kakao.com>
2021-11-10 15:42:39 +0900

    feat: magician grid layout

--- src/Pages/Logs/Magician/index.jsx
+++ src/Pages/Logs/Magician/index.jsx
@@ -1,12 +1,26 @@
 import { Row, Col } from '@Components/Grid';
 import Card from '@Components/Card';
+import EventViewer from '@Components/EventViewer';
+import DummyCardEx from '@Dummy/DummyCardEx';
 
 const Magician = () => {
     return (
         <div id='magician'>
             <Row>
                 <Col xl={4} md={4} mb>
-                    <Card></Card>
+                    <Card>
+                        <DummyCardEx height='300px'></DummyCardEx>
+                    </Card>
+                </Col>
+                <Col xl={8} md={8} mb>
+                    <Card>
+                        <DummyCardEx height='300px'></DummyCardEx>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col xl={12} md={12} mb>
+                    <EventViewer />
                 </Col>
             </Row>
         </div>
