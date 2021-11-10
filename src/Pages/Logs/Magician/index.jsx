478ce08da8313bd8674990e662aaa1d752ea2d45
hyesong kim <wisepine@kakao.com>
2021-11-10 15:59:48 +0900

    feat: magician layout

--- src/Pages/Logs/Magician/index.jsx
+++ src/Pages/Logs/Magician/index.jsx
@@ -1,5 +1,6 @@
 import { Row, Col } from '@Components/Grid';
 import Card from '@Components/Card';
+import CardBodyForm from '@Components/Card/Form';
 import EventViewer from '@Components/EventViewer';
 import DummyCardEx from '@Dummy/DummyCardEx';
 
@@ -9,13 +10,30 @@ const Magician = () => {
             <Row>
                 <Col xl={4} md={4} mb>
                     <Card>
-                        <DummyCardEx height='300px'></DummyCardEx>
+                        <DummyCardEx height='500px'></DummyCardEx>
                     </Card>
                 </Col>
                 <Col xl={8} md={8} mb>
                     <Card>
-                        <DummyCardEx height='300px'></DummyCardEx>
+                        <DummyCardEx height='390px'></DummyCardEx>
                     </Card>
+                    <Row>
+                        <Col md={4} xl={4} mb>
+                            <Card>
+                                <CardBodyForm titleFontColor={'red'} title='보안위험지수' content='1단계' />
+                            </Card>
+                        </Col>
+                        <Col md={4} xl={4} mb>
+                            <Card>
+                                <CardBodyForm titleFontColor={'red'} title='보안위험지수' content='1단계' />
+                            </Card>
+                        </Col>
+                        <Col md={4} xl={4} mb>
+                            <Card>
+                                <CardBodyForm titleFontColor={'red'} title='보안위험지수' content='1단계' />
+                            </Card>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
             <Row>
