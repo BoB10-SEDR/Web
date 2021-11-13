ffbcc604dd44f28ef1b65b98b3654b2269ad769a
hyesong kim <wisepine@kakao.com>
2021-11-13 20:05:38 +0900

    comment: policies.js api 별 주석 추가

--- src/Api/policies.js
+++ src/Api/policies.js
@@ -1,40 +1,48 @@
 import axios from 'axios';
 
+// 전체 대응 정책 리스트 조회
 export const getPolicies = async (page, limit) => {
     const response = await axios.get(`policies?page=${page}&limit=${limit}`);
     return response.data;
 };
 
+// 대응 정책 정보 조회
 export const getPolicy = async idx => {
     const response = await axios.get(`policies/${idx}`);
     return response.data;
 };
 
+// 대응 저책 별 적용 가능한 장비 리스트 조회
 export const getPolicyAvailableDevices = async idx => {
     const response = await axios.get(`policies/${idx}/devices`);
     return response.data;
 };
 
+// 대응 정책 등록
 export const postPolicyRegister = async () => {
     const response = await axios.post(`policies`);
     return response.data;
 };
 
+// 대응 정책 적용
 export const postPolicyActivate = async (idx, deviceIdx) => {
     const response = await axios.post(`policies/${idx}/activate/${deviceIdx}`);
     return response.data;
 };
 
-export const postPolicyInActivate = async (idx, deviceIdx) => {
+// 대응 정책 해제
+export const postPolicyInactivate = async (idx, deviceIdx) => {
     const response = await axios.post(`policies/${idx}/inactivate/${deviceIdx}`);
     return response.data;
 };
 
+// 대응 정책 정보 수정
 export const putPolicyUpdate = async idx => {
     const response = await axios.put(`policies/${idx}`);
     return response.data;
 };
 
+// 대응 정책 정보 삭제
 export const deletePolicy = async idx => {
     const response = await axios.delete(`policies/${idx}`);
     return response.data;
