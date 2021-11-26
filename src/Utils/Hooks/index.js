7c7475816c0c04aa5dbf61f19d0fe5c76c957ff5
hyesong kim <wisepine@kakao.com>
2021-11-27 00:15:49 +0900

    fix: fetcher error

--- src/Utils/Hooks/index.js
+++ src/Utils/Hooks/index.js
@@ -4,10 +4,5 @@ export const fetcher = async (url, ...args) => {
     const response = await axios.get(url, args);
     const { outputs, errors } = response.data;
 
-    if (errors) {
-        const error = new Error('An error occurred while fetching the data.');
-        error.info = errors;
-        throw error;
-    }
     return outputs;
 };
