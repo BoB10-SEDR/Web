65dfb56ebaa13df1aeb0d226c65ec78cd162f4de
hyesong kim <wisepine@kakao.com>
2021-11-30 15:16:35 +0900

    style: 모니터링 페이지 이벤트 뷰어 timestamp width 조정

--- src/Components/Table/data/schema/index.js
+++ src/Components/Table/data/schema/index.js
@@ -63,7 +63,7 @@ export const monitoringTableSchema = [
 ];
 
 export const logMagicianSchema = [
-    { Header: '타임스탬프', accessor: 'create_time', align: 'center', width: '50', isAbleToSort: true },
+    { Header: '타임스탬프', accessor: 'create_time', align: 'center', width: '100', isAbleToSort: true },
     { Header: '기기', accessor: 'device_name', align: 'center', width: '100', isAbleToSort: true },
     { Header: '상태', accessor: 'status', align: 'center', width: '50', isAbleToSort: true },
     { Header: '에이전트 여부', accessor: 'environment', align: 'center', width: '50', isAbleToSort: true },
