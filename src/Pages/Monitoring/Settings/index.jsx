9eab261a8f6a48c15b15f47b32e442d9f987abdf
hyesong kim <wisepine@kakao.com>
2021-12-05 20:43:54 +0900

    feat: 모니터링 페이지 분리

--- src/Pages/Monitoring/Settings/index.jsx
+++ src/Pages/Monitoring/Settings/index.jsx
@@ -1,5 +1,53 @@
+import { useState } from 'react';
+import useSWR, { useSWRConfig } from 'swr';
+import axios from 'axios';
+import Modal from '@Components/Modal';
+import SelectLogFile from '@Components/Modal/ModalContent/SelectLogFile';
+import ManageTable from '@Components/ManageTable';
+import { fetcher } from '@Hooks/';
+
 const Settings = () => {
-    return null;
+    const { mutate } = useSWRConfig();
+    const [page, setPage] = useState(1);
+    const { data: monitoringData = [], error } = useSWR(`/monitoring?page=${page}`, url => fetcher(url));
+
+    const onToggleActivate = async ({ row }) => {
+        const { device_idx: deviceIdx, idx: policyIdx } = row;
+        try {
+            const response = await axios.post(`/monitoring/${deviceIdx}/activate`);
+            mutate(`/monitoring`);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const onToggleInactivate = async ({ row }) => {
+        const { device_idx: deviceIdx, idx: policyIdx } = row;
+        try {
+            const response = await axios.post(`/monitoring/${deviceIdx}/inactivate`);
+            mutate(`/monitoring`);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div id='monitoringSettings' className='page'>
+            <ManageTable
+                title='모니터링 설정'
+                toggleId='idx'
+                toggleValueField='activate'
+                schema='monitoring'
+                browseData={monitoringData}
+                onToggleActivate={onToggleActivate}
+                onToggleInactivate={onToggleInactivate}
+            >
+                <Modal hasButton buttonContent='파일 추가'>
+                    <SelectLogFile />
+                </Modal>
+            </ManageTable>
+        </div>
+    );
 };
 
 export default Settings;
--- src/Pages/Monitoring/index.jsx
+++ src/Pages/Monitoring/index.jsx
@@ -2,7 +2,6 @@ import { Row, Col } from '@Components/Grid';
 import Card from '@Components/Card';
 import MagicianEventViewer from '@Components/MagicianEventViewer';
 import TimeLine from '@Components/Charts/TimeLine';
-import LogMagician from '@Components/LogMagician';
 import MagicianStats from '@Components/MagicianStats';
 import DummyCardEx from '@Dummy/DummyCardEx';
 
@@ -10,14 +9,7 @@ const Monitoring = () => {
     return (
         <div id='magician' className='page'>
             <Row>
-                <Col xl={4} md={4} mb>
-                    <Card title='로그파일 선택'>
-                        <DummyCardEx noRelative height='500px'>
-                            <LogMagician />
-                        </DummyCardEx>
-                    </Card>
-                </Col>
-                <Col xl={8} md={8} mb>
+                <Col>
                     <Card title='로그 실시간 트래픽'>
                         <DummyCardEx height='390px'>
                             <TimeLine />
@@ -29,7 +21,7 @@ const Monitoring = () => {
                 </Col>
             </Row>
             <Row>
-                <Col xl={12} md={12} mb>
+                <Col>
                     <MagicianEventViewer />
                 </Col>
             </Row>
