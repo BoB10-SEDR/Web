92c2fe1430cbb3eb52a575637953344c87c81774
hyesong kim <wisepine@kakao.com>
2021-12-15 09:28:31 +0900

    feat: remover hook

--- src/Utils/Hooks/index.js
+++ src/Utils/Hooks/index.js
@@ -6,3 +6,17 @@ export const fetcher = async (url, ...args) => {
 
     return outputs;
 };
+
+export const remover = (url, ...args) => {
+    const deleteSomething = async () => {
+        try {
+            const reponse = await axios.delete(url, args);
+        } catch (error) {
+            alert('error');
+        }
+    };
+
+    // eslint-disable-next-line no-restricted-globals
+    const isConfirmed = confirm('정말 삭제하시겠습니까?');
+    isConfirmed && deleteSomething();
+};
