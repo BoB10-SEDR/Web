c1de9fa42cd7c36b73dc4d49e3a5c916586d3fb4
hyesong kim <wisepine@kakao.com>
2021-12-16 18:00:54 +0900

    feat: EventViewer count 연동

--- src/Components/MagicianEventViewer/index.jsx
+++ src/Components/MagicianEventViewer/index.jsx
@@ -7,9 +7,19 @@ import dummyLogs from '@Dummy/logMagician';
 const MagicianEventViewer = () => {
     const [page, setPage] = useState(1);
     const [limit, setLimit] = useState(7000);
-    const { data = [], error } = useSWR(`/monitoring/log?page=${page}&limit=${limit}`, url => fetcher(url), {
-        refreshInterval: 60000,
-    });
+    const { data: monitoringData = {}, error } = useSWR(
+        `/monitoring/log?page=${page}&limit=${limit}`,
+        url => fetcher(url),
+        {
+            refreshInterval: 60000,
+            compare: (a, b) => {
+                return JSON.stringify(a?.data) === JSON.stringify(b?.data);
+            },
+        }
+    );
+
+    if (!monitoringData) return <div>loading...</div>;
+    const { count, data = [] } = monitoringData;
 
     return <EventViewer defaultPadding='0 1.25rem 1.25rem 1.25rem' schema='logMagician' data={data} />;
 };
